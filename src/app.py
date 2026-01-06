07db0f98cc370cf6e60893bc6782a352ad46c516
Agr3st <patryk.luty2002@gmail.com>
2026-01-06 21:42:38 +0100

    feature: add main app script

---
+++ src/app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+from src.utils.session import init_session_state
+from src.utils.weather_orchestrator import refresh_weather
+
+init_session_state()
+
+cockpit_page = st.Page(
+    "ui/cockpit.py", title="Kokpit", icon=":material/home:", default=True
+)
+weather_module_page = st.Page(
+    "ui/weather_module.py",
+    title="Moduł Pogodowy",
+    icon=":material/partly_cloudy_day:",
+)
+input_page = st.Page("ui/input.py", title="Dane wejściowe", icon=":material/upload:")
+info_page = st.Page("ui/info.py", title="O systemie", icon=":material/info:")
+session_data_page = st.Page(
+    "ui/session_data.py", title="Dane sesji", icon=":material/browse_activity:"
+)
+
+pg = st.navigation(
+    [cockpit_page, weather_module_page, input_page, info_page, session_data_page]
+)
+pg.run()
+
+
+# orkiestrator modułu pogodowego
+with st.container():
+    refresh_weather(interval_minutes=30)
